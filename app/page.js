d5854fe6afc9a83632c9b1ce145708a30e6adf89
Rock Houinsou <rock.houinsou@epitech.eu>
2025-12-02 05:53:33 +0100

    Add dynamic export to page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import Hero from "./components/layout/hero";
 import { Film, Star, Users, TrendingUp, Play, ChevronRight, Sparkles } from "lucide-react";
